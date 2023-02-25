5cae531791cd1b4eb56bcc2c75c17757b165daf4
★彡[ᴀʜᴍᴇᴅ ᴍᴜᴊᴛᴀʙᴀ]彡★ <107427098+ahmedmujtaba1@users.noreply.github.com>
2023-02-25 18:50:25 +0500

    Add files via upload

---
+++ Assignment 2 (Simple Calculator)/calculator.ts
@@ -0,0 +1,20 @@
+function calculator(number1:number,operator:string,number2:number){
+    if(operator=="+") {
+        let result = number1 + number2
+        console.log(`${number1} ${operator} ${number2} = ${result}`)
+    } else if(operator=="-") {
+        let result = number1 - number2
+        console.log(`${number1} ${operator} ${number2} = ${result}`)
+    } else if(operator=="x") {
+        let result = number1 * number2
+        console.log(`${number1} ${operator} ${number2} = ${result}`)
+    } else if(operator=="/") {
+        let result = number1 / number2
+        console.log(`${number1} ${operator} ${number2} = ${result}`)
+    }
+}
+
+calculator(10, "+", 10)
+calculator(10, "-", 10)
+calculator(10, "x", 10)
+calculator(40, "/", 2)
