b3a418c1ee896c86af55043a8ea2025d7c903095
★彡[ᴀʜᴍᴇᴅ ᴍᴜᴊᴛᴀʙᴀ]彡★ <ahmed.mujtaba11737@hhs.edu.pk>
2023-03-08 11:00:14 +0500

    Add files via upload

---
+++ Tasks/Task 1/Assignment2/task2.ts
@@ -0,0 +1,12 @@
+let fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+// Append a string to the end of the array
+fruits.push("kiwi");
+console.log(fruits);
+// Prepend a string to the beginning of the array
+let fruit2 = ["apple", "banana", "cherry", "date", "elderberry"];
+fruit2.shift();
+console.log(fruit2);
+// Remove a string from a specific index in the array and replace it with another string
+let fruit3 = ["apple", "banana", "cherry", "date", "elderberry"];
+fruit3.splice(2, 1, "mango");
+console.log(fruit3)
