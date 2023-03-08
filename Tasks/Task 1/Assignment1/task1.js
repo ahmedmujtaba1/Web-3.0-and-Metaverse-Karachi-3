4b7ea713b0621ee3975a21acbc822e70b576ac24
★彡[ᴀʜᴍᴇᴅ ᴍᴜᴊᴛᴀʙᴀ]彡★ <ahmed.mujtaba11737@hhs.edu.pk>
2023-03-08 10:29:33 +0500

    Add files via upload

---
+++ Tasks/Task 1/Assignment1/task1.js
@@ -0,0 +1,14 @@
+"use strict";
+let englishMarks = 50;
+let urduMarks = 50;
+let averageMarks = (englishMarks + urduMarks) / 2;
+console.log("Your average Percentage is", averageMarks);
+if (averageMarks >= 80) {
+    console.log("You scored A*");
+}
+else if (averageMarks >= 70 && averageMarks < 80) {
+    console.log("You scored B");
+}
+else if (averageMarks < 60) {
+    console.log("You scored F (Failed)");
+}
