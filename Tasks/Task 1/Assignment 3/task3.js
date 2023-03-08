0f1cb76f702932c03a61f9c35b5a85c5dfeda24c
★彡[ᴀʜᴍᴇᴅ ᴍᴜᴊᴛᴀʙᴀ]彡★ <ahmed.mujtaba11737@hhs.edu.pk>
2023-03-08 11:25:52 +0500

    Add files via upload

---
+++ Tasks/Task 1/Assignment 3/task3.js
@@ -0,0 +1,13 @@
+"use strict";
+function Products(productPrice, discountPercentage) {
+    let discountAmount = productPrice * (discountPercentage / 100);
+    //   console.log(`${discountAmount}%`);
+    let finalPrice = productPrice - discountAmount;
+    if (discountPercentage >= 50) {
+        console.log("Discount percentage is invalid.");
+    }
+    else {
+        console.log(`Original Price is ${productPrice} and after applying ${discountPercentage}%, the discounted price is ${discountAmount} and final pice is ${finalPrice}`);
+    }
+}
+Products(1000, 20);
