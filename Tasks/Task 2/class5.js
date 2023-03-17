f139628cde1e564ebaf3eca3579e7723ac34100b
★彡[ᴀʜᴍᴇᴅ ᴍᴜᴊᴛᴀʙᴀ]彡★ <ahmed.mujtaba11737@hhs.edu.pk>
2023-03-17 16:32:34 +0500

    Add files via upload

---
+++ Tasks/Task 2/class5.js
@@ -0,0 +1,9 @@
+"use strict";
+let arr = [
+    {
+        name: "Ahmed",
+    },
+];
+// Access the object at index 0 and change the value of the name property
+arr[0].name = "Mujtaba";
+console.log(arr);
---
+++ Tasks/Task 2/class5.ts
@@ -0,0 +1,12 @@
+let arr = [
+  {
+    name: "Ahmed",
+  },
+];
+
+// Access the object at index 0 and change the value of the name property
+arr[0].name = "Mujtaba";
+
+console.log(arr); 
+
+
