e970cefd144c3fd736a6672218a83882f778c155
★彡[ᴀʜᴍᴇᴅ ᴍᴜᴊᴛᴀʙᴀ]彡★ <ahmed.mujtaba11737@hhs.edu.pk>
2023-03-25 18:18:36 +0500

    Add files via upload

---
+++ Calculator using inquirer/inquirer_assignment00.js
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+function calculator(number1, operator, number2) {
+    if (operator == "+") {
+        let result = number1 + number2;
+        console.log(`${number1} ${operator} ${number2} = ${result}`);
+    }
+    else if (operator == "-") {
+        let result = number1 - number2;
+        console.log(`${number1} ${operator} ${number2} = ${result}`);
+    }
+    else if (operator == "x") {
+        let result = number1 * number2;
+        console.log(`${number1} ${operator} ${number2} = ${result}`);
+    }
+    else if (operator == "/") {
+        let result = number1 / number2;
+        console.log(`${number1} ${operator} ${number2} = ${result}`);
+    }
+}
+async function getUserInput() {
+    const { myNumber1 } = await inquirer.prompt({
+        message: "Enter your first Number?",
+        name: "myNumber1",
+    });
+    const { myNumber2 } = await inquirer.prompt({
+        message: "Enter your second Number?",
+        name: "myNumber2",
+    });
+    const { myOperator } = await inquirer.prompt({
+        message: "Enter Operator?",
+        name: "myOperator",
+    });
+    calculator(Number(myNumber1), myOperator, Number(myNumber2));
+}
+getUserInput();
